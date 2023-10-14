19b56808893b1203faa36e161905f9e1625483e8
salvalago23 <82556570+salvalago23@users.noreply.github.com>
2023-10-14 22:45:52 +0200

    Changed folder structure and names of scripts inside "src" folder. CSV history.csv column "reward" converted from float to int (not needed but all the other numerical were int)

--- src/1_CSVGeneration.py
+++ src/1_CSVGeneration.py
@@ -1,12 +1,11 @@
 import gymnasium as gym
-#import gym
 from gymnasium import spaces
 
 #import pygame
 #import numpy as np
 
 # Register the environment
-gym.register(id='grid-v0', entry_point='NNmodeledEnv:NNGridWorldEnv')
+gym.register(id='grid-v0', entry_point='environments:CSVGeneratorEnv')
 
 #Maze config
 maze = [
@@ -17,15 +16,12 @@ maze = [
     ['S', '.', '#', '.', '.'],
 ]
 
-grid_model_path = '../data/models/modelo_entorno.h5'
-reward_model_path = '../data/models/modelo_reward.h5'
-
 # Test the environment
-env = gym.make('grid-v0', maze=maze, grid_model_path=grid_model_path, reward_model_path=reward_model_path, max_episode_steps=500)
+env = gym.make('grid-v0',maze=maze, max_episode_steps=500)
 
-with open(f"../data/csv/history4.csv", 'a') as f:
+with open(f"../data/csv/history.csv", 'a') as f:
     #f.write(f"step,y,x,action,next_y,next_x,reward,done\n")
-    for i in range(1):
+    for i in range(3):
         obs, _ = env.reset()
         #env.render()
 
@@ -35,11 +31,11 @@ with open(f"../data/csv/history4.csv", 'a') as f:
             #pygame.event.get()
             action = env.action_space.sample()  # Random action selection
             
-            prev_state = [obs[0], obs[1], action]
+            prev_state = [obs['agent'][0], obs['agent'][1], action]
 
             obs, rew, done, _, _ = env.step(action)
 
-            f.write(f"{t},{prev_state[0]},{prev_state[1]},{prev_state[2]},{obs[0]},{obs[1]},{rew},{done}\n")
+            #f.write(f"{t},{prev_state[0]},{prev_state[1]},{prev_state[2]},{obs['agent'][0]},{obs['agent'][1]},{rew},{done}\n")
             
             t += 1
             #env.render()
--- src/3_NNrun.py
+++ src/3_NNrun.py
@@ -1,11 +1,12 @@
 import gymnasium as gym
+#import gym
 from gymnasium import spaces
 
 #import pygame
 #import numpy as np
 
 # Register the environment
-gym.register(id='grid-v0', entry_point='gridNewEnv:GridWorldEnv')
+gym.register(id='grid-v0', entry_point='environments:NNGridWorldEnv')
 
 #Maze config
 maze = [
@@ -16,12 +17,15 @@ maze = [
     ['S', '.', '#', '.', '.'],
 ]
 
-# Test the environment
-env = gym.make('grid-v0',maze=maze, max_episode_steps=500)
+grid_model_path = '../data/models/modelo_entorno.h5'
+reward_model_path = '../data/models/modelo_reward.h5'
 
-with open(f"../data/csv/history.csv", 'a') as f:
-    #f.write(f"step,y,x,action,next_y,next_x,reward,done\n")
-    for i in range(3):
+# Test the environment
+env = gym.make('grid-v0', maze=maze, grid_model_path=grid_model_path, reward_model_path=reward_model_path, max_episode_steps=500)
+
+with open(f"../data/csv/historyNN.csv", 'a') as f:
+    f.write(f"step,y,x,action,next_y,next_x,reward,done\n")
+    for i in range(1):
         obs, _ = env.reset()
         #env.render()
 
@@ -31,11 +35,11 @@ with open(f"../data/csv/history.csv", 'a') as f:
             #pygame.event.get()
             action = env.action_space.sample()  # Random action selection
             
-            prev_state = [obs['agent'][0], obs['agent'][1], action]
+            prev_state = [obs[0], obs[1], action]
 
             obs, rew, done, _, _ = env.step(action)
 
-            #f.write(f"{t},{prev_state[0]},{prev_state[1]},{prev_state[2]},{obs['agent'][0]},{obs['agent'][1]},{rew},{done}\n")
+            f.write(f"{t},{prev_state[0]},{prev_state[1]},{prev_state[2]},{obs[0]},{obs[1]},{rew},{done}\n")
             
             t += 1
             #env.render()
